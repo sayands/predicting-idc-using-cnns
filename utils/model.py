aec286fb84da2f1fc19936394554d68e2015d080
sayands <sayandsarkar.1997@gmail.com>
2019-04-12 20:10:34 +0530

    Initial Commit on Project Documentation

--- utils/model.py
+++ utils/model.py
@@ -9,17 +9,17 @@ from keras.callbacks import ReduceLROnPlateau
 # Function to define model architecture
 def init_model(num_classes):
     # Defining Model
-    model = Sequential()
+    model = Sequential() # Initialise model 
     model.add(Conv2D(32, kernel_size=(3, 3),
                      activation='relu',
-                     input_shape=(50, 50, 3),strides=2))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPool2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(num_classes, activation='softmax'))
+                     input_shape=(50, 50, 3),strides=2)) # 24 * 24 * 32
+    model.add(Conv2D(64, (3, 3), activation='relu')) # 22 * 22 * 64
+    model.add(MaxPool2D(pool_size=(2, 2))) # 11 * 11 * 64
+    model.add(Dropout(0.25)) # 11 * 64 * 64
+    model.add(Flatten()) # 7744
+    model.add(Dense(128, activation='relu')) # 128
+    model.add(Dropout(0.5)) # 128
+    model.add(Dense(num_classes, activation='softmax')) # 2
 
     # Defining Optimizer
     optimizer = Adadelta(lr = 0.75)
